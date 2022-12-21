4581e2572ec0c25eb39f192ff2cde8db2f6bae36
Chris Weyland <github@nextmail.cc>
2022-12-21 12:01:08 +0300

    documentation

--- unshuffle/unshuffle.py
+++ unshuffle/unshuffle.py
@@ -1,35 +1,16 @@
 #!/usr/bin/env python3
-"""A module to "decrypt" shuffled words based on the frequency of occurance in everyday language.
+"""A module to "decrypt" shuffled words based on the frequency of occurance
+in everyday language.
 
-This module demonstrates documentation as specified by the `Google Python
-Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
+There is no further analysis of the grammar or meaning of the word. Shuffled
+strings are simply replaced based on the generated dictionary.
 
-Example:
-    Examples can be given using either the ``Example`` or ``Examples``
-    sections. Sections support any reStructuredText formatting, including
-    literal blocks::
-
-        $ python example_google.py
-
-Section breaks are created by resuming unindented text. Section breaks
-are also implicitly created anytime a new section starts.
-
-Attributes:
-    module_level_variable1 (int): Module level variables may be documented in
-        either the ``Attributes`` section of the module docstring, or in an
-        inline docstring immediately following the variable.
-
-        Either form is acceptable, but the two should not be mixed. Choose
-        one convention to document module level variables and be consistent
-        with it.
-
-Todo:
-    * For module TODOs
-    * You have to also use ``sphinx.ext.todo`` extension
-
-.. _Google Python Style Guide:
-   http://google.github.io/styleguide/pyguide.html
+Example::
+    shuffled_text = "grubereheCes" 
+    txt = Text(Dict('dict.txt'))
+    txt.shuffled = shuffled_text
+    # Prints: Cheeseburger 
+    print(txt)
 """
 import re
 
